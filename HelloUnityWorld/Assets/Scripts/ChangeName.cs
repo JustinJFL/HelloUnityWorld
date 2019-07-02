707d3969e9f96eda7a101cb01fbfe27ff0ba0c19
justinjfl <jsnell@knights.ucf.edu>
2019-07-01 23:30:34 -0400

    Created Unity Project and created script for user input to change text on screen when spacebar is pressed.

---
+++ HelloUnityWorld/Assets/Scripts/ChangeName.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ChangeName : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        TextMeshProUGUI tmp = GetComponent<TextMeshProUGUI>();
+        if (Input.GetKeyDown("space"))
+        {
+            tmp.SetText("Hello World, My name is Justin Snell!");
+            Debug.Log("space key pressed");
+        }
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+    }
+}
